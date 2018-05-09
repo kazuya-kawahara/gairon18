c701732a8910f20e958fb422f2949b37154688df
kazuya-kawahara <kawahara.k.ad@m.titech.ac.jp>
2018-05-09 17:45:18 +0900

    dnaFinished

--- 04/src/main/resources/color.scala
+++ 04/src/main/resources/color.scala
@@ -9,3 +9,12 @@ case class MakeParent(
 
 :paste
 //プログラムは以下に書くこと
+
+def color :
+(ParentTree) => List[Symbol]
+=
+(tree) => {
+ tree match {
+  case Empty => Nil
+  case MakeParent(Nil,n,d,e) => e::Nil
+  case MakeParent(r::rest,n,d,e) => e::color(r)++color(MakeParent(rest,n,d,e))}}
--- 04/src/main/resources/dna.scala
+++ 04/src/main/resources/dna.scala
@@ -1,1 +1,14 @@
 :paste
+
+def DNAprefix :
+(List[Symbol],List[Symbol]) => Boolean
+=
+(l1,l2) => {
+ l1 match {
+ case Nil => true
+ case r1::rest1 => 
+  l2 match {
+  case Nil => false
+  case r2::rest2 => 
+   if (r1==r2) {DNAprefix(rest1,rest2)}
+   else {false}}}}
